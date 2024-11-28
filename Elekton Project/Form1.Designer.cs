cc4add6b9dd626b36aea9296ebb75d0dfce2ca5e
Fourrier.Krisztina <Fourrier.Krisztina@C16-03>
2024-11-28 21:17:22 +0100

    Add project files.

---
+++ Elekton Project/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿
+namespace Elekton_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SlideUp = new System.Windows.Forms.PictureBox();
+            this.SlideLeft = new System.Windows.Forms.PictureBox();
+            this.SlideRight = new System.Windows.Forms.PictureBox();
+            this.Battery = new System.Windows.Forms.PictureBox();
+            this.Plus = new System.Windows.Forms.PictureBox();
+            this.Minus = new System.Windows.Forms.PictureBox();
+            this.pictureBox7 = new System.Windows.Forms.PictureBox();
+            this.Switch = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Electron = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.SlideDown = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.SlideUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlideLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlideRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Battery)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Switch)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Electron)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlideDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SlideUp
+            // 
+            this.SlideUp.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SlideUp.Location = new System.Drawing.Point(64, 37);
+            this.SlideUp.Name = "SlideUp";
+            this.SlideUp.Size = new System.Drawing.Size(678, 378);
+            this.SlideUp.TabIndex = 0;
+            this.SlideUp.TabStop = false;
+            // 
+            // SlideLeft
+            // 
+            this.SlideLeft.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.SlideLeft.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SlideLeft.Location = new System.Drawing.Point(154, 97);
+            this.SlideLeft.Name = "SlideLeft";
+            this.SlideLeft.Padding = new System.Windows.Forms.Padding(0, 1, 1, 0);
+            this.SlideLeft.Size = new System.Drawing.Size(495, 2);
+            this.SlideLeft.TabIndex = 1;
+            this.SlideLeft.TabStop = false;
+            // 
+            // SlideRight
+            // 
+            this.SlideRight.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.SlideRight.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.SlideRight.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SlideRight.Location = new System.Drawing.Point(146, 336);
+            this.SlideRight.Name = "SlideRight";
+            this.SlideRight.Size = new System.Drawing.Size(455, 2);
+            this.SlideRight.TabIndex = 2;
+            this.SlideRight.TabStop = false;
+            // 
+            // Battery
+            // 
+            this.Battery.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Battery.Location = new System.Drawing.Point(581, 345);
+            this.Battery.Name = "Battery";
+            this.Battery.Size = new System.Drawing.Size(100, 50);
+            this.Battery.TabIndex = 3;
+            this.Battery.TabStop = false;
+            // 
+            // Plus
+            // 
+            this.Plus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Plus.Location = new System.Drawing.Point(591, 336);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(26, 11);
+            this.Plus.TabIndex = 4;
+            this.Plus.TabStop = false;
+            // 
+            // Minus
+            // 
+            this.Minus.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Minus.Location = new System.Drawing.Point(642, 336);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(26, 11);
+            this.Minus.TabIndex = 5;
+            this.Minus.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            this.pictureBox7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox7.Location = new System.Drawing.Point(99, 58);
+            this.pictureBox7.Name = "pictureBox7";
+            this.pictureBox7.Size = new System.Drawing.Size(100, 117);
+            this.pictureBox7.TabIndex = 6;
+            this.pictureBox7.TabStop = false;
+            // 
+            // Switch
+            // 
+            this.Switch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Switch.Location = new System.Drawing.Point(303, 58);
+            this.Switch.Name = "Switch";
+            this.Switch.Size = new System.Drawing.Size(85, 117);
+            this.Switch.TabIndex = 7;
+            this.Switch.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(316, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Up";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(316, 138);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(55, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Down";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(494, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Electroncount: 368";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Location = new System.Drawing.Point(325, 81);
+            this.label2.Name = "label2";
+            this.label2.Padding = new System.Windows.Forms.Padding(2);
+            this.label2.Size = new System.Drawing.Size(32, 19);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "12V";
+            // 
+            // Electron
+            // 
+            this.Electron.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Electron.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Electron.Location = new System.Drawing.Point(642, 212);
+            this.Electron.Name = "Electron";
+            this.Electron.Size = new System.Drawing.Size(17, 16);
+            this.Electron.TabIndex = 13;
+            this.Electron.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox4.Location = new System.Drawing.Point(649, 97);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(2, 242);
+            this.pictureBox4.TabIndex = 14;
+            this.pictureBox4.TabStop = false;
+            // 
+            // SlideDown
+            // 
+            this.SlideDown.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.SlideDown.Location = new System.Drawing.Point(146, 175);
+            this.SlideDown.Name = "SlideDown";
+            this.SlideDown.Size = new System.Drawing.Size(2, 164);
+            this.SlideDown.TabIndex = 15;
+            this.SlideDown.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SlideDown);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.Electron);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Switch);
+            this.Controls.Add(this.pictureBox7);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.Battery);
+            this.Controls.Add(this.SlideRight);
+            this.Controls.Add(this.SlideLeft);
+            this.Controls.Add(this.SlideUp);
+            this.Name = "Form1";
+            this.Text = "Electron";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.SlideUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlideLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlideRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Battery)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Minus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Switch)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Electron)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SlideDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox SlideUp;
+        private System.Windows.Forms.PictureBox SlideLeft;
+        private System.Windows.Forms.PictureBox SlideRight;
+        private System.Windows.Forms.PictureBox Battery;
+        private System.Windows.Forms.PictureBox Plus;
+        private System.Windows.Forms.PictureBox Minus;
+        private System.Windows.Forms.PictureBox pictureBox7;
+        private System.Windows.Forms.PictureBox Switch;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox Electron;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox SlideDown;
+        public System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Elekton Project/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*A feladat hogy szimuláljuk ahogy az elektronok végigmennek a vezetéken. Az elektron az akkumulátor negatív feléről indul a pozitív felé. Az áramkörben van egy feszültésg szabályzó és
+ * egy fogyasztó. A feszültség szabályzóig konstans sebességgel haladnak az elektronok. A feszültség szabályzó az utána lévő részre beállítja az elektron sebességét.
+Az akkumulátor 24 Voltos.
+Az alapértelmezett feszültség érték 12 Volt amit egyesével lehet fel és le módosítani.
+A maximális feszültség nem lehet nagyobb az akku feszültségénél. 
+Miután egy elektron áthaladt a feszültség szabályzón, a sebessége változatlan marad.
+A sebességet arányosan kell beállítani. Ha 12 Volton megy az elektron, akkor a 24 Voltos sebesség 50 százalékával kell haladnia.
+A mozgás timer intervallumja fixen 16ms.
+Van egy számláló ami visszajelzést nyújt arról hány elektron jutott vissza az akkuba.
+Az elektronok fél másodpercenként jönnek létre */
+
+
+namespace Elekton_Project
+{
+    public partial class Form1 : Form
+    {
+        Timer electronTimer = new Timer();
+        
+        int voltSize = 12;
+        int xDirection = 0;
+        int yDirection = -1;
+        int electronCount = 368;
+     
+        int upVoltage = 24;
+        int downVoltage = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Feszültség";
+            Start();
+
+        }
+        void Start() 
+        {
+        
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+        void startTimer() 
+        {
+            electronTimer.Interval = 24;
+            feszultsegEvent();
+            moveEvent();
+            electronTimer.Start();
+            //electronTimer.Tick += electronSpeed;
+        }
+        void feszultsegEvent()
+          
+        {
+            electronTimer.Interval = 24;
+            
+            electronTimer.Start();
+
+        }
+        void moveEvent() 
+        {
+           // upVoltage.Click += upVolt;
+            //downVoltage.Click += downVoltage;
+        }
+       
+        void switchEvent(object s, EventArgs e) 
+        { 
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (voltSize < 24)
+            {
+                voltSize++;
+                label2.Text = voltSize.ToString() + "V";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (voltSize > 1)
+            {
+                voltSize--;
+                label2.Text = voltSize.ToString() + "V";
+            }
+        }
+    }
+}
